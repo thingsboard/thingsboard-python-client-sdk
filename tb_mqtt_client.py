00f4c1c1c52795f0b7e61101ddfc86755dd59c4f
serhiilikl <serhii.likh@gmail.com>
2019-03-12 12:44:01 +0200

    fin subscribe

--- tb_mqtt_client.py
+++ tb_mqtt_client.py
@@ -14,17 +14,19 @@ log.addHandler(errorHandler)
 
 
 class TB_client:
+    callback = None
     is_connected = False
+    sub_dict = {}
     def __init__(self, host, token):
         self.client = paho.Client()
         self.host = host
         self.client.username_pw_set(token)
         self.callback = None
 
-        def _on_log(client, userdata, level, buf):
+        def on_log(client, userdata, level, buf):
             log.info(buf)
 
-        def _on_connect(client, userdata, flags, rc, *extra_params):
+        def on_connect(client, userdata, flags, rc, *extra_params):
             result_codes = {
                 1: "incorrect protocol version",
                 2: "invalid client identifier",
@@ -42,39 +44,40 @@ class TB_client:
                 else:
                     log.error("connection FAIL with unknown error")
 
-        def _on_disconnect(client, userdata, rc):
+        def on_disconnect(client, userdata, rc):
             if rc == 0:
                 log.info("disconnect SUCCESS")
             else:
                 log.error("disconnect FAIL with error code %i" % rc)
 
-        def _on_publish(client, userdata, result):
+        def on_publish(client, userdata, result):
             log.info("data published")
 
-        def _on_message(client, userdata, message):
+        def on_message(client, userdata, message):
             log.info(message.payload.decode("utf-8"))
             log.info(message.topic)
             if message.topic == 'v1/devices/me/attributes':
-                self.sub_message_processing({message.topic: message.payload.decode("utf-8")})
+
+                self.callback(message.payload.decode("utf-8"))
 
 
-        def _on_subscribe(client, userdata, mid, granted_qos):
+        def on_subscribe(client, userdata, mid, granted_qos):
             log.info("Subscribe")
             #TODO добавить больше информации
 
-        def _on_unsubscribe(client, userdata, mid, granted_qos):
+        def on_unsubscribe(client, userdata, mid, granted_qos):
             log.info("Unsubscribe")
             #TODO добавить больше информации
 
 
 
-        self.client.on_disconnect = _on_disconnect
-        self.client.on_connect = _on_connect
-        self.client.on_log = _on_log
-        self.client.on_publish = _on_publish
-        self.client.on_message = _on_message
-        self.client.on_subscribe = _on_subscribe
-        self.client.on_unsubscribe = _on_unsubscribe
+        self.client.on_disconnect = on_disconnect
+        self.client.on_connect = on_connect
+        self.client.on_log = on_log
+        self.client.on_publish = on_publish
+        self.client.on_message = on_message
+        self.client.on_subscribe = on_subscribe
+        self.client.on_unsubscribe = on_unsubscribe
 
     def loop(self):
         return self.client.loop()
@@ -100,22 +103,57 @@ class TB_client:
         if blocking: info.wait_for_publish()
 
 
-    def __subscribe_to_attributes_blocking(self, callback, *attributes):
-        # как мне использовать колбек?
-        self.client.subscribe('v1/devices/me/attributes', qos=2)
+    def unsubscribe_to_attributes(self, subscription_id):
+        empty_keys = []
+        for attribute in self.sub_dict.keys():
+            for x in self.sub_dict[attribute]:
+                if x["subscription_id"] == subscription_id:
+                    self.sub_dict[attribute].remove(x)
+            if self.sub_dict[attribute] == []:
+                empty_keys.append(attribute)
+        for key in empty_keys:
+            del self.sub_dict[key]
 
+    def process_callback(self, id, callback):
+        subs_list = []
+        for key in self.sub_dict.keys():
+            for x in self.sub_dict[key]:
+                if x["subscription_id"] == id:
+                    subs_list.append(key)
+        for x in subs_list:
+            if callback.get(x):
+                return((x, callback.get(x)))
 
-    def subscribe_to_attributes(self, callback, attr_name):
-        pass
-    #def unsubscribe_to_attributes(self, subscriptionId):
-
-    def sub_message_processing(self):
-        pass
 
     def subscribe_to_attributes(self, callback, key="*"):
-        #TODO впилить валидацию аттрибутов
-        subscription_id = None
-        self.attributes = key
         self.client.subscribe('v1/devices/me/attributes', qos=2)
-        self.sub_message_processing = callback
+        self.callback = callback
+
+        def find_max_sub_id():
+            res = 1
+            for key in self.sub_dict.keys():
+                for item in self.sub_dict[key]:
+                    if item["subscription_id"] > res:
+                        res = item["subscription_id"]
+            return res
+
+        subscription_id = find_max_sub_id()
+        inst = {
+            "subscription_id": subscription_id,
+            "callback": callback.__name__
+        }
+
+        def sub(inst, attribute):
+            # subscribe to everything
+            if attribute == "*":
+                for key in self.sub_dict.keys():
+                    if inst not in self.sub_dict[key]:
+                        self.sub_dict[key].append(inst)
+            # if attribute doesnot exist create it with subscription
+            elif attribute not in self.sub_dict.keys():
+                self.sub_dict.update({attribute: [inst]})
+            # if attribute exists create subscription
+            else:
+                self.sub_dict[attribute].append(inst)
+        sub(inst, key)
         return(subscription_id)
