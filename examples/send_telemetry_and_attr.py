fdfd47e43ce5d5100cc40844f22c3c467d597c0f
serhiilikl <serhii.likh@gmail.com>
2019-03-15 17:12:16 +0200

    updated send telemetry example to fit all usecases

--- examples/send_telemetry_and_attr.py
+++ examples/send_telemetry_and_attr.py
@@ -4,6 +4,8 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 telemetry = {"temp": 1001}
+telemetry = [{"temp": 1001}, {"kek":"lol"}]
+telemetry = {"ts":1451649600512, "values":{"key1":"value1", "key2":"value2"}}
 attributes = {"firmwareVersion": "v2.3.2", "temp": 1}
 
 client = tb.TbClient("demo.thingsboard.io", "v5cgxxXGHvuFwdxENEc7")
@@ -11,16 +13,3 @@ client.connect()
 client.send_telemetry(telemetry, blocking=1)
 client.send_attributes(attributes)
 client.disconnect()
-
-
-
-#использовать треды ++++++
-
-
-
-#{"temp": 1001}
-#вложеннные джейсоны нет
-#другие возможные ошибки валидации
-
-
-#вложеннные джейсоны нет
