1b09ae39d8dc954f9f30da4749966f11f4a340b7
serhiilikl <serhii.likh@gmail.com>
2019-03-15 17:17:36 +0200

    upd example

--- examples/subscription_to_attrs.py
+++ examples/subscription_to_attrs.py
@@ -2,12 +2,14 @@ import logging, logging.handlers
 import tb_mqtt_client as tb
 logging.basicConfig(level=logging.INFO)
 
+ACCESS_TOKEN = "v5cgxxXGHvuFwdxENEc7"
+HOST = "demo.thingsboard.io"
 
 def callback(result):
     print(result)
 
 
-client = tb.TbClient("demo.thingsboard.io", "v5cgxxXGHvuFwdxENEc7")
+client = tb.TbClient(HOST, ACCESS_TOKEN)
 client.connect()
 sub_id = client.subscribe(callback, "ololo")
 client.unsubscribe(sub_id)
