50381d69b2d3e36951fa0df42a2fa04339fa0384
serhiilikl <serhii.likh@gmail.com>
2019-03-15 12:43:39 +0200

    pep8 fixes

--- examples/hardware_specs_sender.py
+++ examples/hardware_specs_sender.py
@@ -14,15 +14,14 @@ def freq_cb(value=None):
     uploadFrequency = int(value["uploadFrequency"])
 
 
-def on_server_side_rpc_request (requestId, requestBody):
-    if requestBody["method"] == "getCPULoad":
-        client.respond(requestId, {"CPU percent": psutil.cpu_percent()})
-    if requestBody["method"] == "getMemoryUsage":
-        client.respond(requestId, {"Memory": psutil.virtual_memory().percent})
+def on_server_side_rpc_request(request_id, request_body):
+    if request_body["method"] == "getCPULoad":
+        client.respond(request_id, {"CPU percent": psutil.cpu_percent()})
+    if request_body["method"] == "getMemoryUsage":
+        client.respond(request_id, {"Memory": psutil.virtual_memory().percent})
+
 
 client.set_server_side_rpc_request_handler(on_server_side_rpc_request)
-
-
 client.connect()
 client.subscribe(to_rpc=True)
 client.subscribe(callback=freq_cb, key="uploadFrequency", quality_of_service=2)
@@ -30,9 +29,3 @@ while True:
     client.send_telemetry(dumps({"CPU percent": psutil.cpu_percent()}))
     client.send_telemetry(dumps({"Memory": psutil.virtual_memory().percent}))
     time.sleep(uploadFrequency)
-
-
-
-
-
-
