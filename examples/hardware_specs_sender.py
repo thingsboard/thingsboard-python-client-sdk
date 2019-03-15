57de94dbdb1ccd6bb52877353421616f8f765208
serhiilikl <serhii.likh@gmail.com>
2019-03-15 13:01:19 +0200

    pep8 fixes

--- examples/hardware_specs_sender.py
+++ examples/hardware_specs_sender.py
@@ -7,6 +7,8 @@ logging.basicConfig(level=logging.INFO)
 uploadFrequency = 5
 
 client = TbClient("demo.thingsboard.io", "v5cgxxXGHvuFwdxENEc7")
+
+
 def freq_cb(value=None):
     global uploadFrequency
     uploadFrequency = int(value["uploadFrequency"])
@@ -17,6 +19,7 @@ def on_server_side_rpc_request(request_id, request_body):
         client.respond(request_id, {"CPU percent": psutil.cpu_percent()})
     if request_body["method"] == "getMemoryUsage":
         client.respond(request_id, {"Memory": psutil.virtual_memory().percent})
+
 
 client.set_server_side_rpc_request_handler(on_server_side_rpc_request)
 client.connect()
