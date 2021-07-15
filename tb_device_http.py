af2bbf7330375fa4bb3f7822ff625cbdab1b40d4
Christian Loelkes <christian.loelkes@gmail.com>
2021-07-15 23:49:29 +0200

    add send and request methods for attributes

--- tb_device_http.py
+++ tb_device_http.py
@@ -20,6 +20,21 @@ class TBHTTPClient:
         response = self.session.post(f'{self.api_base_url}/{endpoint}', json=data)
         response.raise_for_status()
 
+    def get_data(self, params: dict, endpoint: str) -> dict:
+        """Send data to the ThingsBoard HTTP API."""
+        response = self.session.get(f'{self.api_base_url}/{endpoint}', params=params)
+        response.raise_for_status()
+        return response.json()
+
     def send_telemetry(self, telemetry: dict):
         """Publish telemetry to the ThingsBoard HTTP Device API."""
         self.publish_data(telemetry, 'telemetry')
+
+    def send_attributes(self, attributes: dict):
+        """Send attributes to the ThingsBoard HTTP Device API."""
+        self.publish_data(attributes, 'attributes')
+
+    def request_attributes(self, client_keys: list = None, shared_keys: list = None) -> dict:
+        """Request attributes from the ThingsBoard HTTP Device API."""
+        params = {'client_keys': client_keys, 'shared_keys': shared_keys}
+        return self.get_data(params=params, endpoint='attributes')
