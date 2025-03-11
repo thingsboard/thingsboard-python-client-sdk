069c13b22133883c1222cd92c7df88d676e2b58a
timyr220 <tim4ka2006@gmail.com>
2025-03-11 14:45:15 +0200

    Fix TBDeviceMqttClient tests

--- tests/split_message_tests.py
+++ tests/split_message_tests.py
@@ -85,7 +85,7 @@ class TestSendSplitMessageRetry(unittest.TestCase):
 
     @patch('tb_device_mqtt.sleep', autospec=True)
     @patch('tb_device_mqtt.log.warning', autospec=True)
-    def test_send_split_message_queue_size_retry(self, mock_log_warning, mock_sleep):
+    def test_send_split_message_queue_size_retry(self, mock_log_warning):
         part = {'datapoints': 3, 'message': {"foo": "bar"}}
         kwargs = {}
         timeout = 10
