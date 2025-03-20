ae09db7777c0611c9c5243b37c536025c2ba6ed5
imbeacon <ibarkov.ua@gmail.com>
2025-03-20 07:32:39 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
-VERSION = "1.11.0"
+VERSION = "1.12.0"
 
 setup(
     version=VERSION,
