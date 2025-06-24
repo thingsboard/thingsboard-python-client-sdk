14af70b5cd34757344cc6b63e1c1a906344d803b
samson0v <vitalikbidochka@gmail.com>
2025-06-24 15:39:45 +0300

    Added provisioning and firmware update examples

---
+++ examples/device/firmware_update.py
@@ -0,0 +1,46 @@
+#  Copyright 2025 ThingsBoard
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# Example script to update firmware using the DeviceClient
+
+import asyncio
+
+from tb_mqtt_client.common.config_loader import DeviceConfig
+from tb_mqtt_client.service.device.client import DeviceClient
+
+
+firmware_received = asyncio.Event()
+
+
+async def firmware_update_callback(_, payload):
+    print(f"Firmware update payload received: {payload}")
+    firmware_received.set()
+
+
+async def main():
+    config = DeviceConfig()
+    config.host = "localhost"
+    config.access_token = "YOUR_ACCESS_TOKEN"
+
+    client = DeviceClient(config)
+    await client.connect()
+
+    await client.update_firmware(on_received_callback=firmware_update_callback)
+    await firmware_received.wait()
+
+    await client.stop()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
