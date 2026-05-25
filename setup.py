5bcf524c4dc844825b164b0f6905a504244a8f7e
Ilyar <ilyar.software@gmail.com>
2026-05-25 19:36:31 +0200

    feat: expose MQTT proxy configuration

--- setup.py
+++ setup.py
@@ -35,4 +35,4 @@ setup(
     long_description_content_type="text/markdown",
     python_requires=">=3.9",
     packages=["."],
-    install_requires=['tb-paho-mqtt-client>=2.1.2', 'requests>=2.31.0', 'orjson'])
+    install_requires=['tb-paho-mqtt-client>=2.1.2', 'requests>=2.31.0', 'orjson', 'PySocks'])
